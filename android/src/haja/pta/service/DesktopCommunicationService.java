bb5821e222e1816abb57a29e7d0fcd23601dcdd1
haja <h.jagenteufel@gmail.com>
2013-01-23 23:40:34 +0100

    android: moved handler to service class

--- android/src/haja/pta/service/DesktopCommunicationService.java
+++ android/src/haja/pta/service/DesktopCommunicationService.java
@@ -1,18 +1,19 @@
-/**
- * 
- */
 package haja.pta.service;
 
 import haja.pta.R;
+import haja.pta.common.dto.IAmAlive;
 
 import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.net.Socket;
-import java.net.UnknownHostException;
 
 import android.app.Service;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.IBinder;
+import android.util.Log;
+import android.widget.Toast;
 
 
 /**
@@ -23,9 +24,44 @@ public class DesktopCommunicationService extends Service {
 
     private static final String PREFS_NAME = "desktop.config";
 
+    protected static final String TAG = "DesktopCommunicationService";
+
     private String _host;
     private int _port;
 
+    private Runnable _serviceRunnable = new Runnable() {
+
+        private boolean _running = true;
+
+        @Override
+        public void run() {
+            try {
+                // open connection to desktop and send/receive some data
+                Socket socket = new Socket(_host, _port);
+
+                ObjectOutputStream outputStream = new ObjectOutputStream(
+                        socket.getOutputStream());
+                ObjectInputStream inputStream = new ObjectInputStream(
+                        socket.getInputStream());
+                outputStream.writeObject(new IAmAlive("android"));
+                outputStream.flush();
+
+                while(_running) {
+                    IAmAlive desktopAlive = (IAmAlive) inputStream.readObject();
+                    Log.i(TAG, desktopAlive.getMessage());
+                }
+
+                socket.close();
+            } catch(IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            } catch(ClassNotFoundException e) {
+                e.printStackTrace();
+            }
+
+            DesktopCommunicationService.this.stopSelf();
+        }
+    };
 
     @Override
     public IBinder onBind(Intent arg0) {
@@ -33,27 +69,26 @@ public class DesktopCommunicationService extends Service {
     }
 
     @Override
-    public int onStartCommand(Intent intent, int flags, int startId) {
-        // open connection to desktop and send/receive some data
+    public void onCreate() {
+        Toast.makeText(this, "service starting", Toast.LENGTH_SHORT).show();
+
         SharedPreferences settings = getSharedPreferences(PREFS_NAME, 0);
         _host = settings.getString("desktop_host", getResources()
                 .getString(R.string.desktop_host_default));
         _port = settings.getInt("desktop_port", getResources()
                 .getInteger(R.integer.desktop_port_default));
 
-        try {
-            Socket socket = new Socket(_host, _port);
-            new Thread(new DesktopCommunicationHandler(socket)).start();
 
-        } catch(UnknownHostException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        } catch(IOException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-
-        return START_STICKY;
+        Thread thread = new Thread(null, _serviceRunnable, "ptaClient");
+        thread.start();
     }
 
+    @Override
+    public void onDestroy() {
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        return START_STICKY;
+    }
 }
