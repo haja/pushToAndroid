f837f1b91114c7cb279a90fac91032904ca48878
haja <h.jagenteufel@gmail.com>
2013-01-23 01:14:15 +0100

    desktop: open connection to client and send/receive data

--- desktop/src/main/java/haja/pta/communication/ClientSetupHandler.java
+++ desktop/src/main/java/haja/pta/communication/ClientSetupHandler.java
@@ -1,7 +1,13 @@
 package haja.pta.communication;
 
+import haja.pta.common.dto.IAmAlive;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.net.Socket;
 
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 
 
@@ -14,6 +20,7 @@ public class ClientSetupHandler implements Runnable {
     private Socket _socket;
     @Autowired
     private IPhoneCommunicationManagement _phoneCommManagement;
+    private Logger _log = Logger.getLogger(ClientSetupHandler.class);
 
     public ClientSetupHandler(Socket clientSocket) {
         _socket = clientSocket;
@@ -26,6 +33,27 @@ public class ClientSetupHandler implements Runnable {
     public void run() {
         IClientConnectionHandler handler = new ClientConnectionHandler(_socket);
         _phoneCommManagement.addClient(handler);
+        
+        // receive alive and send back
+        try {
+            ObjectOutputStream outputStream = new ObjectOutputStream(_socket.getOutputStream());
+            ObjectInputStream inputStream = new ObjectInputStream(_socket.getInputStream());
+            IAmAlive clientAlive = (IAmAlive) inputStream.readObject();
+            _log.info("client alive: " + clientAlive.getMessage());
+            outputStream.writeObject(new IAmAlive("desktop"));
+            outputStream.flush();
+            _log.info("written to client");
+            
+            _socket.close();
+            _log.info("socket to client closed");
+        } catch(IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch(ClassNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        
     }
 
 }
--- desktop/src/main/java/haja/pta/communication/ConnectionListenerTcp.java
+++ desktop/src/main/java/haja/pta/communication/ConnectionListenerTcp.java
@@ -9,7 +9,9 @@ import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.config.AutowireCapableBeanFactory;
 
 
 /**
@@ -21,15 +23,22 @@ public class ConnectionListenerTcp implements IConnectionListener {
     private boolean _keepRunning = true;
     @Autowired
     private Config _config;
+    private Logger _log = Logger.getLogger(ConnectionListenerTcp.class);
+    
+    @Autowired
+    private AutowireCapableBeanFactory _beanFactory;
 
     @Override
     public void run() {
         try {
+            _log.info("config: " + _config);
             ServerSocket serverSocket = new ServerSocket(_config.getInt("desktop_port"));
             
             while(_keepRunning) {
                 Socket clientSocket = serverSocket.accept();
-                new Thread(new ClientSetupHandler(clientSocket)).start();
+                ClientSetupHandler clientSetupHandler = new ClientSetupHandler(clientSocket);
+                _beanFactory.autowireBean(clientSetupHandler);
+                new Thread(clientSetupHandler).start();
             }
             
             serverSocket.close();
