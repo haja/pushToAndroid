a5b1188341e827b468d15e918bbbfe943e76559c
haja <h.jagenteufel@gmail.com>
2013-01-24 21:15:16 +0100

    common: bugfix, open ObjectOutputStream before ObjectInputStream

--- common/src/main/java/haja/pta/common/communication/GenericStreamTcp.java
+++ common/src/main/java/haja/pta/common/communication/GenericStreamTcp.java
@@ -8,48 +8,55 @@ import java.net.Socket;
 
 /**
  * @author Harald Jagenteufel
- *
+ * 
  */
-public class GenericStreamTcp<RequestT, ResponseT> implements IGenericStream<RequestT, ResponseT> {
+public class GenericStreamTcp<RequestT, ResponseT> implements
+        IGenericStream<RequestT, ResponseT> {
 
-	private ObjectOutputStream _oos;
-	private ObjectInputStream _ois;
-	private Socket _socket;
+    private ObjectOutputStream _oos;
+    private ObjectInputStream _ois;
+    private Socket _socket;
 
-	public GenericStreamTcp(InetAddress host, int port) throws IOException {
-		this(new Socket(host, port));
-	}
-	
-	public GenericStreamTcp(Socket socket) throws IOException {
-	    _socket = socket;
-		_ois = new ObjectInputStream(
-		        _socket.getInputStream());
-		_oos = new ObjectOutputStream(_socket.getOutputStream());
+    public GenericStreamTcp(InetAddress host, int port) throws IOException {
+        this(new Socket(host, port));
     }
 
-	/* (non-Javadoc)
+    public GenericStreamTcp(Socket socket) throws IOException {
+        _socket = socket;
+        _oos = new ObjectOutputStream(_socket.getOutputStream());
+        _ois = new ObjectInputStream(
+                _socket.getInputStream());
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
      * @see haja.pta.communication.IGenericStream#close()
      */
-	@Override
+    @Override
     public void close() throws IOException {
-		_socket.close();
+        _socket.close();
     }
 
-	/* (non-Javadoc)
+    /*
+     * (non-Javadoc)
+     * 
      * @see haja.pta.communication.IGenericStream#write(RequestT)
      */
-	@Override
+    @Override
     public void write(RequestT request) throws IOException {
-		_oos.writeObject(request);
-		_oos.flush();
+        _oos.writeObject(request);
+        _oos.flush();
     }
 
-	/* (non-Javadoc)
+    /*
+     * (non-Javadoc)
+     * 
      * @see haja.pta.communication.IGenericStream#read()
      */
-	@Override
+    @Override
     public ResponseT read() throws IOException {
-		try {
+        try {
             return (ResponseT) _ois.readObject();
         } catch(ClassNotFoundException e) {
             e.printStackTrace();
